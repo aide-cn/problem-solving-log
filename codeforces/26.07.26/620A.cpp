7ce455d6fd91229938ffdda610cfec6ec0acbde5
Aide Rahman <aiderahmann@gmail.com>
2026-07-26 20:06:16 +0800

    codeforces

---
+++ codeforces/26.07.26/620A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+
+void solve() {
+    ll n;
+    cin >> n;
+    cout << 25;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
